dd943615151cbe6c908708d4c66c9db200fedad4
paras <paras7taneja@gmail.com>
2025-09-04 23:46:48 +0530

    added routes

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,17 @@ router.get("/hello",async()=>{
     console.log(hello)
 });
 
+router.post("/userAPI", async(req,res)=>{
 
+    let {userName,password} = req.body; // means username=req.body.userName
+
+    userName = await User.findOne({})
+
+    if(userName){
+
+    }
+
+})
 
 
 
