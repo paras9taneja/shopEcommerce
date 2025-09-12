9d9833dd834a0f096a467ac32e689c3d0b7c6782
paras <paras7taneja@gmail.com>
2025-09-12 23:28:31 +0530

    added a new route

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,37 @@
-const express= require("express")
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
+// const user=require(u)
 
 
-router.get("/hello",async()=>{
-   return  console.log("welcome to the routerrrr");
-   
+router.get("/hello", async () => {
+    return console.log("welcome to the routerrrr");
+
     console.log(hello)
 });
 
-router.post("/userAPI", async(req,res)=>{
+router.post("/userAPI", async (req, res) => {
 
-    let {userName,password} = req.body; // means username=req.body.userName
+    let { userName, password } = req.body; // means username=req.body.userName
 
-    userName = await User.findOne({userName:req.body.userName});
+    userName = await User.findOne({ userName: req.body.userName });
 
-    if(userName){
-        return res.json({message:"user already exists"})
+    if (userName) {
+        return res.json({ message: "user already exists" })
     };
 
-    try{
-            
+    try {
+        const user = new User({
+            userName: req.body.userName,
+            password: req.body.password
+        });
+
+        await user.save();
+        return res.json({ user })
     }
-    catch(error){
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send({ message: "internal error" })
 
     };
 
