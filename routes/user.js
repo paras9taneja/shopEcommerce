f0af7efe70ba2cfa44f18818e0ce56efe3c0d670
paras <paras7taneja@gmail.com>
2025-12-17 23:39:44 +0530

    added password hashing

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
 // const user=require(u)
 
-router.get("/", async (req,res) => {
+router.get("/", async (req, res) => {
     return res.send("hello")
     return console.log("welcome to the server");
 
- 
+
 });
 
 
@@ -21,28 +23,35 @@ router.post("/createUserAPI", async (req, res) => {
 
     let { userName, email, password } = req.body; // means username=req.body.userName
 
-    const existingUser = await User.findOne({ userName: req.body.userName });
+    const existingUser = await User.findOne({ email: req.body.email });
 
     if (existingUser) {
+
         return res.json({ message: "user already exists" })
     };
+   const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
 
     try {
         const user = new User({
-            userName:userName,
+            userName: userName,
             email: email,
-            password: password
+            password: hash
         });
 
+        
+        
+        
         await user.save();
-        return res.json({ user })
+        const {password,...others} = user._doc;
+        return res.json({ others })
     }
     catch (error) {
         console.error(error.message);
         res.status(500).send({ message: "internal error" })
 
     };
-
+    // console.log("hello2")
 
 })
 
